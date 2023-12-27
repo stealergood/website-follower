df2ccb6ff521d054ebbcdaf23081b21f92d60af5
Denny Edy Saputra <ncstealer@gmail.com>
2023-12-27 22:43:35 +0700

    Changes to be committed: 	modified:   package-lock.json 	modified:   package.json 	modified:   src/components/form/index.jsx

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
+import { useForm } from "react-hook-form"
 import PropTypes from "prop-types";
 // import PhoneInput from "react-phone-number-input";
 
 const formatRupiah = (number) => {
   if (number === null || isNaN(number)) {
-    return ''; // Return an empty string if the number is null or NaN
+    return '';
   }
 
   const formatter = new Intl.NumberFormat('id-ID', {
@@ -16,7 +17,7 @@ const formatRupiah = (number) => {
 
 const fee = (number) => {
   if (number === null || isNaN(number)) {
-    return ''; // Return an empty string if the number is null or NaN
+    return '';
   }
 
   const formatter = new Intl.NumberFormat('id-ID', {
@@ -28,7 +29,7 @@ const fee = (number) => {
 
 const total = (number, fee) => {
   if (number === null || isNaN(number)) {
-    return ''; // Return an empty string if the number is null or NaN
+    return '';
   }
 
   const formatter = new Intl.NumberFormat('id-ID', {
@@ -44,32 +45,46 @@ export default function Form({theme, buttonBuy}) {
     buttonBuy: PropTypes.object.isRequired
   };
 
+  const { register, handleSubmit, formState: { errors } } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(JSON.stringify(data));
+  };
+
 
   return (
     <div className="w-full min-h-screen p-10">
       <div className="w-full flex flex-col items-center md:items-start md:flex-row gap-3">
         <div className="w-full h-full rounded-lg bg-white p-10">
-          <form action="" className="flex flex-col gap-10">
+          <div className="flex flex-col gap-10">
             <div>
               <div className="mb-5">
                 <h1 className="text-2xl text-slate-600 font-bold">Target</h1>
                 <p className="text-slate-400">Pastikan username/link yang Anda input sudah benar dan akun tidak berstatus private.</p>
               </div>
-              <input type="text" placeholder="Target Username @" className="h-12 text-lg w-full border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
+              <input {...register('target', { required: true })} type="text" placeholder="Target Username @" className="h-12 text-lg w-full border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
+              {errors.target && <span className="text-red-500">Harap isi kolom ini!</span>}
             </div>
             <div>
               <div className="mb-5">
                 <h1 className="text-2xl text-slate-600 font-bold">Informasi Kontak</h1>
                 <p className="text-slate-400">Pastikan anda mengisi seluruh informasi kontak.</p>
               </div>
-              <input type="text" placeholder="Nama Lengkap" className="h-12 w-full text-lg border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
+              <input {...register('fullname', { required: true })} type="text" placeholder="Nama Lengkap" className="h-12 w-full text-lg border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
+              {errors.fullname && <span className="text-red-500">Harap isi kolom ini!</span>}
               <div className="flex gap-5 mt-5">
-                <input type="text" placeholder="Alamat Email" className="h-12 w-full text-lg border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
-                <input type="text" placeholder="No. Whatsapp" className="h-12 w-full text-lg border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
+                <div className="w-full">  
+                  <input {...register('email', { required: true })} type="text" placeholder="Alamat Email" className="h-12 w-full text-lg border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
+                  {errors.email && <span className="text-red-500">Harap isi kolom ini!</span>}
+                </div>
+                <div className="w-full">
+                  <input {...register('phone', { required: true })} type="text" placeholder="No. Whatsapp" className="h-12 w-full text-lg border-2 rounded-md bg-slate-100 border-gray-200 outline-none p-2"/>
+                  {errors.phone && <span className="text-red-500">Harap isi kolom ini!</span>}
+                </div>
                 {/* <PhoneInput defaultCountry="ID" placeholder="No. Whatsapp" value={value} onChange={setValue} PhoneInputCountryFlag-height={1}/> */}
               </div>
             </div>
-          </form>
+          </div>
         </div>
         <div className="w-full md:w-3/5 h-full rounded-lg flex flex-col gap-5">
           <div className={`flex justify-center items-center rounded-lg w-full h-16 ${theme === 'light' ? 'bg-navlight' : 'bg-navdark'}`}>
@@ -93,7 +108,20 @@ export default function Form({theme, buttonBuy}) {
               <p className="text-lg font-bold text-slate-600">{total(buttonBuy.price, buttonBuy.price * 0.05)}</p>
             </div>
           </div>
-          <button className={`w-full h-16 rounded-lg text-white font-bold ${theme === 'light' ? 'bg-navlight' : 'bg-navdark'}`}>CHECKOUT</button>
+          <button
+            onClick={() => {
+              if (Object.keys(buttonBuy).length === 0) {
+                alert("Harap pilih produk!");
+              } else {
+                handleSubmit(onSubmit)();
+              }
+            }}
+            type="submit"
+            className={`w-full h-16 rounded-lg text-white font-bold ${theme === 'light' ? 'bg-navlight' : 'bg-navdark'}`}
+          >
+            CHECKOUT
+          </button>
+
         </div>
       </div>
     </div>
